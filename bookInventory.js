bef8a15f0ce54fb00a9455b7565e18f2d48a0231
radeshshiwlochan <radesh0430@gmail.com>
2016-07-29 15:01:09 -0400

    inital commit that searches an array or objects

---
+++ bookInventory.js
@@ -0,0 +1,39 @@
+var booksOnHand = [
+
+	{
+		"author" : "Dan Brown",
+		"name" : "Angels and Demons",
+		"yearPublished" : "2001"
+	},
+	{
+		"author" : "J.R Tolken",
+		"name" : "Lord of the Rings the fellowship of the Ring",
+		"yearPublished" : "1989"
+	},
+	{
+		"author" : "J.R Tolken",
+		"name" : "Lord of the Rings the Two Towers",
+		"yearPublished" : "1991"
+	},
+	{
+		"author" : "J.R Tolken",
+		"name" : "Lord of the Rings, Return of the King",
+		"yearPublished" : "1992"
+	},
+	{
+		"author" : "J.R Tolken",
+		"name" : "Harry Potter and the Sorcerer Stone",
+		"yearPublished" : "2001"
+	},
+	{
+		"author" : "J.R Tolken",
+		"name" : "Harry Potter and the Chambers of Secrets",
+		"yearPublished" : "2002"
+	},
+	{
+		"author" : "J.R Tolken",
+		"name" : "Harry Potter and the Prisoner of Azaban",
+		"yearPublished" : "2003"
+	}
+
+];
--- server.js
+++ server.js
@@ -1,10 +1,61 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient
+var Fuse = require('fuse.js');
+//enter the name of the module here .
+var menusModule = require('./bookInventory.js');
+var booksOnHand = menusModule.booksOnHand;
 const app = express();
 var db;
 
-MongoClient.connect('mongodb://localhost:27017/yoda',function(err, database) {
+var booksOnHand = [
+
+	{
+		"author" : "Dan Brown",
+		"name" : "Angels and Demons",
+		"yearPublished" : "2001"
+	},
+	{
+		"author" : "J.R Tolken",
+		"name" : "Lord of the Rings the fellowship of the Ring",
+		"yearPublished" : "1989"
+	},
+	{
+		"author" : "J.R Tolken",
+		"name" : "Lord of the Rings the Two Towers",
+		"yearPublished" : "1991"
+	},
+	{
+		"author" : "J.R Tolken",
+		"name" : "Lord of the Rings, Return of the King",
+		"yearPublished" : "1992"
+	},
+	{
+		"author" : "J.R Tolken",
+		"name" : "Harry Potter and the Sorcerer Stone",
+		"yearPublished" : "2001"
+	},
+	{
+		"author" : "J.R Tolken",
+		"name" : "Harry Potter and the Chambers of Secrets",
+		"yearPublished" : "2002"
+	},
+	{
+		"author" : "J.R Tolken",
+		"name" : "Harry Potter and the Prisoner of Azaban",
+		"yearPublished" : "2003"
+	}
+
+];
+
+var options = {
+	keys: ['author', 'name', 'yearPublished'],
+	caseSensitive: false
+}
+
+
+//enter the name of the database here.
+MongoClient.connect('mongodb://localhost:27017/Books',function(err, database) {
 	if(err)
 		return console.log(err);
 	db = database;
@@ -19,15 +70,28 @@ app.get('/', function(req, res) {
 	res.sendFile(__dirname + '/index.html')
 });
 
-app.post('/quotes', function(req, res) {
-	db.collection('quotes').save(req.body, function(err, result) {
+app.post('/search', function(req, res) {
+	MongoClient.connect('mongodb://localhost:27017/Books',function(err, db) {
 		if(err)
 			return console.log(err);
-		console.log('saved to database');
-		res.redirect('/');
-	})
-})
 
-// app.listen(3000, function() {
-// 	console.log("listening on 3000");
-// });
+	var searchString = req.body.name;
+	// //enter the name of the collection here.
+	// var returnCursor = db.collection('inventory').find().toArray();
+	// console.log("=========================================");
+	// console.log("found " );
+	// printjson (returnCursor[0]);
+	var fusionObject = new Fuse(booksOnHand,options);
+    var result = fusionObject.search(searchString);
+
+ 		// for(var j = 0; j < returnCursor.length; j++) {
+ 		// 	console.log(returnCursor[j]);
+ 		// }
+
+    for(var i = 0; i < result.length; i++) {
+    	console.log(result[i]);
+    }
+    });
+	
+});
+
