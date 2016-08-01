fef79294a0bca5ba35924dd7e67b1ed8c85eaa74
radeshshiwlochan <radesh0430@gmail.com>
2016-08-01 15:02:26 -0400

    returns all the data in the collection

--- server.js
+++ server.js
@@ -5,49 +5,11 @@ var Fuse = require('fuse.js');
 //enter the name of the module here .
 var menusModule = require('./bookInventory.js');
 var booksOnHand = menusModule.booksOnHand;
+var url = 'mongodb://localhost:27017/Books';
 const app = express();
 var db;
 
-// var booksOnHand = [
-
-// 	{
-// 		"author" : "Dan Brown",
-// 		"name" : "Angels and Demons",
-// 		"yearPublished" : "2001"
-// 	},
-// 	{
-// 		"author" : "J.R Tolken",
-// 		"name" : "Lord of the Rings the fellowship of the Ring",
-// 		"yearPublished" : "1989"
-// 	},
-// 	{
-// 		"author" : "J.R Tolken",
-// 		"name" : "Lord of the Rings the Two Towers",
-// 		"yearPublished" : "1991"
-// 	},
-// 	{
-// 		"author" : "J.R Tolken",
-// 		"name" : "Lord of the Rings, Return of the King",
-// 		"yearPublished" : "1992"
-// 	},
-// 	{
-// 		"author" : "J.R Tolken",
-// 		"name" : "Harry Potter and the Sorcerer Stone",
-// 		"yearPublished" : "2001"
-// 	},
-// 	{
-// 		"author" : "J.R Tolken",
-// 		"name" : "Harry Potter and the Chambers of Secrets",
-// 		"yearPublished" : "2002"
-// 	},
-// 	{
-// 		"author" : "J.R Tolken",
-// 		"name" : "Harry Potter and the Prisoner of Azaban",
-// 		"yearPublished" : "2003"
-// 	}
-
-// ];
-
+//the keys for what to search.
 var options = {
 	keys: ['author', 'name', 'yearPublished'],
 	caseSensitive: false
@@ -55,45 +17,44 @@ var options = {
 
 
 //enter the name of the database here.
-MongoClient.connect('mongodb://localhost:27017/Books',function(err, database) {
+MongoClient.connect(url,function(err, database) {
 	if(err)
 		return console.log(err);
 	db = database;
-	app.listen(3000, function() {
-		console.log('listening on 3000');
+	app.listen(8000, function() {
+		console.log('listening on 8000');
 	});
 });
 
 app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/', function(req, res) {
-	res.sendFile(__dirname + '/index.html')
+	res.sendFile(__dirname + '/index.html');
 });
 
 app.post('/search', function(req, res) {
-	MongoClient.connect('mongodb://localhost:27017/Books',function(err, db) {
-		if(err)
-			return console.log(err);
-
 	var searchString = req.body.name;
-	// //enter the name of the collection here.
-	// var returnCursor = db.collection('inventory').find().toArray();
-	// console.log("=========================================");
-	// console.log("found " );
-	// printjson (returnCursor[0]);
+	console.log("this is what the user searched ", searchString);
+	db.collection('inventory').find({}).toArray(function(err, results) {
+		if(err) 
+			console.log(err);
+		console.log(results);
+
+	});
+	// for(var i = 0; i < results; i++)
+	// 	console.log(results[i]);
+	// console.log("end of the while loop")
+    /*
+    //This finds a book, when the user enters the name of the book.
 	var fusionObject = new Fuse(booksOnHand,options);
-    var result = fusionObject.search(searchString);
+    var results = db.collection('inventory').findOne({name: searchString})
+    	.then(function(items) {
+    		console.log(items);
+    		callback(items);
+    	});
+    */	
+    //var output = fusionObject.search(searchString);
 
- 		// for(var j = 0; j < returnCursor.length; j++) {
- 		// 	console.log(returnCursor[j]);
- 		// }
-
- 		
-
-    for(var i = 0; i < result.length; i++) {
-    	console.log(result[i]);
-    }
-    });
-	
+   
 });
 
